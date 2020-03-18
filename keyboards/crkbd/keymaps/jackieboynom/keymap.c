6511e620aee7cb58681792c5861529dec1f54ef6
Ko-Chen Chen <jack141094@gmail.com>
2020-03-18 02:35:27 -0500

    changed oled display

--- keyboards/crkbd/keymaps/jackieboynom/keymap.c
+++ keyboards/crkbd/keymaps/jackieboynom/keymap.c
@@ -117,18 +117,17 @@ void render_blank_line(void) {
 void render_first_line(uint8_t modifiers) {
     static const char PROGMEM corne_logo[] = {0x20, 0x20, 0x80, 0x81, 0x82, 0x83, 0x84, 0x20, 0x20, 0};
 
-    static const char PROGMEM adjust_layer[] = {0x91, 0x92, 0x93, 0x20, 0};
-    static const char PROGMEM i3_layer[] = {0x94, 0x95, 0x96, 0x20, 0};
-    static const char PROGMEM code_layer[] = {0x97, 0x98, 0x99, 0x20, 0};
-    static const char PROGMEM qwerty_layer[] = {0x9a, 0x9b, 0x9c, 0x20, 0};
-    static const char PROGMEM fn_layer[] = {0x9d, 0x9e, 0x9f, 0x20, 0};
+    static const char PROGMEM adjust_layer[] = {0x85, 0x86, 0x87, 0x20, 0};
+    static const char PROGMEM code_layer[] = {0x88, 0x89, 0x8a, 0x20, 0};
+    static const char PROGMEM qwerty_layer[] = {0x8b, 0x8c, 0x8d, 0x20, 0};
+    static const char PROGMEM fn_layer[] = {0x8e, 0x8f, 0x90, 0x20, 0};
 
-    static const char PROGMEM ctrl_off[] = {0x89, 0x8a, 0};
-    static const char PROGMEM ctrl_on[] = {0x8b, 0x8c, 0};
-    static const char PROGMEM alt_off[] = {0x85, 0x86, 0};
-    static const char PROGMEM alt_on[] = {0x87, 0x88, 0};
-    static const char PROGMEM shift_off[] = {0x8d, 0x8e, 0x20, 0x20, 0};
-    static const char PROGMEM shift_on[] = {0x8f, 0x90, 0x20, 0x20, 0};
+    static const char PROGMEM ctrl_off[] = {0x91, 0x92, 0};
+    static const char PROGMEM alt_off[] = {0x93, 0x94, 0};
+    static const char PROGMEM shift_off[] = {0x95, 0x96, 0x20, 0x20, 0};
+    static const char PROGMEM ctrl_on[] = {0x97, 0x98, 0};
+    static const char PROGMEM alt_on[] = {0x99, 0x9a, 0};
+    static const char PROGMEM shift_on[] = {0x9b, 0x9c, 0x20, 0x20, 0};
 
     oled_write_P(corne_logo, false);
 
@@ -164,18 +163,17 @@ void render_first_line(uint8_t modifiers) {
 void render_second_line(uint8_t modifiers) {
     static const char PROGMEM corne_logo[] = {0x20, 0x20, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0x20, 0x20, 0};
 
-    static const char PROGMEM adjust_layer[] = {0xb1, 0xb2, 0xb3, 0x20, 0};
-    static const char PROGMEM i3_layer[] = {0xb4, 0xb5, 0xb6, 0x20, 0};
-    static const char PROGMEM code_layer[] = {0xb7, 0xb8, 0xb9, 0x20, 0};
-    static const char PROGMEM qwerty_layer[] = {0xba, 0xbb, 0xbc, 0x20, 0};
-    static const char PROGMEM fn_layer[] = {0xbd, 0xbe, 0xbf, 0x20, 0};
+    static const char PROGMEM adjust_layer[] = {0xa5, 0xa6, 0xa7, 0x20, 0};
+    static const char PROGMEM code_layer[] = {0xa8, 0xa9, 0xaa, 0x20, 0};
+    static const char PROGMEM qwerty_layer[] = {0xab, 0xac, 0xad, 0x20, 0};
+    static const char PROGMEM fn_layer[] = {0xae, 0xaf, 0xb0, 0x20, 0};
 
-    static const char PROGMEM ctrl_off[] = {0xa9, 0xaa, 0};
-    static const char PROGMEM ctrl_on[] = {0xab, 0xac, 0};
-    static const char PROGMEM alt_off[] = {0xa5, 0xa6, 0};
-    static const char PROGMEM alt_on[] = {0xa7, 0xa8, 0};
-    static const char PROGMEM shift_off[] = {0xad, 0xae, 0x20, 0x20, 0};
-    static const char PROGMEM shift_on[] = {0xaf, 0xb0, 0x20, 0x20, 0};
+    static const char PROGMEM ctrl_off[] = {0xb1, 0xb2, 0};
+    static const char PROGMEM alt_off[] = {0xb3, 0xb4, 0};
+    static const char PROGMEM shift_off[] = {0xb5, 0xb6, 0x20, 0x20, 0};
+    static const char PROGMEM ctrl_on[] = {0xb7, 0xb8, 0};
+    static const char PROGMEM alt_on[] = {0xb9, 0xba, 0};
+    static const char PROGMEM shift_on[] = {0xbb, 0xbc, 0x20, 0x20, 0};
 
     oled_write_P(corne_logo, false);
 
@@ -211,18 +209,17 @@ void render_second_line(uint8_t modifiers) {
 void render_third_line(uint8_t modifiers) {
     static const char PROGMEM corne_logo[] = {0x20, 0x20, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0x20, 0x20, 0};
 
-    static const char PROGMEM adjust_layer[] = {0xd1, 0xd2, 0xd3, 0x20, 0};
-    static const char PROGMEM i3_layer[] = {0xd4, 0xd5, 0xd6, 0x20, 0};
-    static const char PROGMEM code_layer[] = {0xd7, 0xd8, 0xd9, 0x20, 0};
-    static const char PROGMEM qwerty_layer[] = {0xda, 0xdb, 0xdc, 0x20, 0};
-    static const char PROGMEM fn_layer[] = {0xdd, 0xde, 0xdf, 0x20, 0};
+    static const char PROGMEM adjust_layer[] = {0xc5, 0xc6, 0xc7, 0xdd, 0};
+    static const char PROGMEM code_layer[] = {0xc8, 0xc9, 0xca, 0xdd, 0};
+    static const char PROGMEM qwerty_layer[] = {0xcb, 0xcc, 0xcd, 0xdd, 0};
+    static const char PROGMEM fn_layer[] = {0xce, 0xcf, 0xd0, 0xdd, 0};
 
-    static const char PROGMEM alt_off[] = {0xc5, 0xc6, 0};
-    static const char PROGMEM alt_on[] = {0xc7, 0xc8, 0};
-    static const char PROGMEM ctrl_off[] = {0xc9, 0xca, 0};
-    static const char PROGMEM ctrl_on[] = {0xcb, 0xcc, 0};
-    static const char PROGMEM shift_off[] = {0xcd, 0xce, 0x20, 0x20, 0};
-    static const char PROGMEM shift_on[] = {0xcf, 0xd0, 0x20, 0x20, 0};
+    static const char PROGMEM ctrl_off[] = {0xd1, 0xd2, 0};
+    static const char PROGMEM alt_off[] = {0xd3, 0xd4, 0};
+    static const char PROGMEM shift_off[] = {0xd5, 0xd6, 0x20, 0x20, 0};
+    static const char PROGMEM ctrl_on[] = {0xd7, 0xd8, 0};
+    static const char PROGMEM alt_on[] = {0xd9, 0xda, 0};
+    static const char PROGMEM shift_on[] = {0xdb, 0xdc, 0x20, 0x20, 0};
 
     oled_write_P(corne_logo, false);
 
