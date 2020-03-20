dd8a605777285ed94188df276b30b67caf7aeca0
Ko-Chen Chen <jack141094@gmail.com>
2020-03-20 18:21:55 -0500

    new keymap

--- keyboards/crkbd/keymaps/jackieboynom/keymap.c
+++ keyboards/crkbd/keymaps/jackieboynom/keymap.c
@@ -56,11 +56,11 @@ CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
 
   [_CODE] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-       KC_GRV, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,\
+        KC_NO, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_NO, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,\
+        KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                        KC_NO, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,\
+        KC_NO,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                       KC_GRV, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,MO(_ADJUST),KC_TRNS,  KC_TRNS,MO(_CODE),  KC_NO \
                                       //`--------------------------'  `--------------------------'
