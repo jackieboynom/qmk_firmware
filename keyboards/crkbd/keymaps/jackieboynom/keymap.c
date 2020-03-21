5437a74f07222683d9ca696144bca48d4605e4f9
Ko-Chen Chen <jack141094@gmail.com>
2020-03-20 23:07:54 -0500

    changed keym

--- keyboards/crkbd/keymaps/jackieboynom/keymap.c
+++ keyboards/crkbd/keymaps/jackieboynom/keymap.c
@@ -38,7 +38,7 @@ CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,SFT_T(KC_ENT),\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, MO(_FN),  KC_SPC,     KC_SPC,MO(_CODE),  KC_NO \
+                                          KC_LALT, MO(_FN),  KC_SPC,     KC_SPC,MO(_CODE),MO(_FN) \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -46,24 +46,24 @@ CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,   KC_NO, KC_WH_U, KC_BTN1, KC_MS_U, KC_BTN2,                      KC_HOME,   KC_UP,  KC_END, KC_PGUP,KC__MUTE,   KC_NO,\
+      KC_TRNS,   KC_NO, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,                      KC_PGUP, KC_HOME,   KC_UP,  KC_END,   KC_NO,KC__MUTE,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,   KC_NO, KC_WH_D, KC_MS_L, KC_MS_D, KC_MS_R,                      KC_LEFT, KC_DOWN,KC_RIGHT, KC_PGDN,KC__VOLDOWN,KC__VOLUP,\
+      KC_TRNS,   KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_PGDN, KC_LEFT, KC_DOWN,KC_RIGHT,KC__VOLDOWN,KC__VOLUP,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_TRNS, MO(_FN), KC_TRNS,    KC_TRNS,MO(_ADJUST),KC_NO \
+                                          KC_TRNS, MO(_FN), KC_TRNS,    KC_TRNS,MO(_ADJUST),MO(_FN) \
                                       //`--------------------------'  `--------------------------'
   ),
 
   [_CODE] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_NO, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,\
+        KC_NO, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_TILD,  KC_GRV,  KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_TILD, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,\
+        KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_UNDS, KC_MINS, KC_PLUS,  KC_EQL, KC_PIPE, KC_BSLS,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                       KC_GRV, KC_MINS,  KC_EQL, KC_LBRC, KC_RBRC, KC_BSLS,\
+        KC_NO,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_TRNS,MO(_ADJUST),KC_TRNS,  KC_TRNS,MO(_CODE),  KC_NO \
-                                      //`--------------------------'  `--------------------------'
+                                      //`--------------------------'  `--------------------------' 
   ),
 
   [_ADJUST] = LAYOUT( \
