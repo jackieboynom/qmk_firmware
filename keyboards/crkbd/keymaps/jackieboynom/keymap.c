fc94d38ae5c4efc741d0129a73c57055db836056
Ko-Chen Chen <jack141094@gmail.com>
2020-03-27 17:45:58 -0500

    changed keymap

--- keyboards/crkbd/keymaps/jackieboynom/keymap.c
+++ keyboards/crkbd/keymaps/jackieboynom/keymap.c
@@ -38,7 +38,7 @@ CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,SFT_T(KC_ENT),\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LALT, MO(_FN),  KC_SPC,     KC_SPC,MO(_CODE),MO(_FN) \
+                                          KC_LGUI, KC_LALT, MO(_FN),     KC_SPC,MO(_CODE),MO(_FN) \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -46,11 +46,11 @@ CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,   KC_NO, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,                      KC_PGUP, KC_HOME,   KC_UP,  KC_END,   KC_NO,KC__MUTE,\
+      KC_TRNS,   KC_NO, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,   KC_NO,KC__MUTE,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,   KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_PGDN, KC_LEFT, KC_DOWN,KC_RIGHT,KC__VOLDOWN,KC__VOLUP,\
+      KC_TRNS,   KC_NO, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_HOME,  KC_END, KC_PGUP, KC_PGDN,KC__VOLDOWN,KC__VOLUP,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_TRNS, MO(_FN), KC_TRNS,    KC_TRNS,MO(_ADJUST),MO(_FN) \
+                                          KC_LGUI, KC_TRNS, MO(_FN),    KC_TRNS,MO(_ADJUST),MO(_FN) \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -62,7 +62,7 @@ CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_TRNS,MO(_ADJUST),KC_TRNS,  KC_TRNS,MO(_CODE),  KC_NO \
+                                          KC_LGUI, KC_TRNS,MO(_ADJUST), KC_TRNS,MO(_CODE),MO(_ADJUST) \
                                       //`--------------------------'  `--------------------------' 
   ),
 
