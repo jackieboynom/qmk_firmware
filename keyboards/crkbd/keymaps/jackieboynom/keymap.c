b4ee4e661e1f9682bccc9dc753cef11d1d1a9be6
Ko-Chen Chen <jack141094@gmail.com>
2020-04-10 12:44:35 -0500

    changed keymap for crkbd jackieboynom

--- keyboards/crkbd/keymaps/jackieboynom/keymap.c
+++ keyboards/crkbd/keymaps/jackieboynom/keymap.c
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,LT(_FN,KC_F),KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
+CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,SFT_T(KC_ENT),\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -56,11 +56,11 @@ CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,LT(_FN,KC_F),KC_G,                     
 
   [_CODE] = LAYOUT( \
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-        KC_NO, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_TILD,  KC_GRV,  KC_DEL,\
+        KC_NO, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_UNDS, KC_MINS, KC_PLUS,  KC_EQL, KC_PIPE, KC_BSLS,\
+        KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_GRV, KC_TILD, KC_LBRC, KC_LCBR, KC_RBRC, KC_RCBR,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_TRNS,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_LPRN, KC_LBRC, KC_LCBR, KC_RPRN, KC_RCBR, KC_RBRC,\
+      KC_TRNS,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_UNDS, KC_MINS, KC_PLUS,  KC_EQL, KC_BSLS, KC_PIPE,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, KC_TRNS,MO(_ADJUST), KC_TRNS,MO(_CODE),MO(_ADJUST) \
                                       //`--------------------------'  `--------------------------' 
@@ -74,7 +74,7 @@ CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,LT(_FN,KC_F),KC_G,                     
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, RGB_SPD,   KC_NO,                        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                            KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO \
+                                            KC_NO,   KC_NO,MO(_ADJUST),   KC_NO,MO(_ADJUST),MO(_ADJUST) \
                                       //`--------------------------'  `--------------------------'
   )
 };
