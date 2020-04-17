addad0ed6560e0958b8b56e9ffa04b229a4f3eb5
Ko-Chen Chen <jack141094@gmail.com>
2020-04-17 13:42:59 -0500

    changed keymap and swap master

--- keyboards/crkbd/keymaps/jackieboynom/keymap.c
+++ keyboards/crkbd/keymaps/jackieboynom/keymap.c
@@ -58,7 +58,7 @@ CTL_T(KC_ESC),    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                     
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_NO, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_GRV, KC_TILD, KC_LBRC, KC_LCBR, KC_RBRC, KC_RCBR,\
+        KC_NO,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC, KC_TILD,  KC_GRV,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_TRNS,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_UNDS, KC_MINS, KC_PLUS,  KC_EQL, KC_BSLS, KC_PIPE,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
@@ -89,7 +89,7 @@ void matrix_init_user(void) {
 
 #ifdef OLED_DRIVER_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-    if (is_master) {
+    if (!is_master) {
         return OLED_ROTATION_180; 
     }
     return rotation;
@@ -234,7 +234,7 @@ void render_third_line(uint8_t modifiers) {
 }
 
 void oled_task_user(void) {
-    if (is_master) {
+    if (!is_master) {
         //render_blank_line();
         render_first_line(get_mods()|get_oneshot_mods());
         render_second_line(get_mods()|get_oneshot_mods());
