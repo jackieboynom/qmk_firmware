2907c6b33ee95621549cc6aa1fdac4a8ea95e6c1
Ko-Chen Chen <jack141094@gmail.com>
2020-04-23 23:01:56 -0500

    new keymap for crkbd jackieboynom

--- keyboards/crkbd/keymaps/jackieboynom/keymap.c
+++ keyboards/crkbd/keymaps/jackieboynom/keymap.c
@@ -33,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
        KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,  KC_BSPC,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-       KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
+      KC_LCTL,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      KC_LCTL,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH, KC_RSFT,\
+      KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ESC,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_LGUI,MO(FNUC),SFT_T(KC_ENT),KC_SPC,MO(CODE), KC_RALT \
+                                          KC_LGUI,MO(FNUC),  KC_ENT,     KC_SPC,MO(CODE), KC_RALT \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -45,11 +45,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         KC_F1,   KC_F2, KC_BTN1, KC_MS_U, KC_BTN2, KC_WH_U,                      KC_HOME,  KC_END, KC_PGUP, KC_PGDN, XXXXXXX, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-        KC_F3,   KC_F4, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT, XXXXXXX, XXXXXXX,\
+      _______,   KC_F3, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,KC__MUTE, XXXXXXX,\
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      _______,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,                       KC_F10,  KC_F11,  KC_F12,KC__MUTE,KC__VOLDOWN,KC__VOLUP,\
+      _______,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,                        KC_F9,  KC_F10,  KC_F11,  KC_F12,KC__VOLDOWN,KC__VOLUP,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX,MO(FNUC), _______,    _______, MO(ADJ), _______ \
+                                          XXXXXXX,MO(FNUC), XXXXXXX,    XXXXXXX, MO(ADJ), _______ \
                                       //`--------------------------'  `--------------------------'
   ),
 
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       _______,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                      KC_UNDS, KC_MINS, KC_PLUS,  KC_EQL, KC_BSLS, KC_PIPE,\
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          XXXXXXX,MO(ADJ),  _______,    _______,MO(CODE), XXXXXXX \
+                                          XXXXXXX,MO(ADJ),  XXXXXXX,    XXXXXXX,MO(CODE), XXXXXXX \
                                       //`--------------------------'  `--------------------------' 
   ),
 
